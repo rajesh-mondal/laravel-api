861dd385f37ca8d6c36f2466af9928bc4d404f45
rajesh-mondal <rajesh01749@gmail.com>
2022-10-20 10:32:05 +0600

    all articles are displayed

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    function getAllArticles(){
+        return Article::all();
+    }
 }
